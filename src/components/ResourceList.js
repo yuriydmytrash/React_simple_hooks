53aacbac726d0720716076ce609d5713e608d00d
dmytryy <dmytrash.yura@gmail.com>
2019-10-09 16:42:43 -0700

    refactoring with useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -3,14 +3,21 @@ import axios from 'axios';
 
 // class ResourceList extends React.Component {
 //   // state = { resources: [] };
-const ResourceList = () => {
+const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async () => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+  const fetchResource = async (resource) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
-    this.setState({ resource: response.data });
-  }
+    setResources(response.data);
+    // this.setState({ resource: response.data });
+  };
+
+  useEffect(() => {
+    fetchResource(resource)
+  }, [])
+
+
   return <div>{resources.length}</div>;
 }
 
